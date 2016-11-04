f1d4832c80a569c008a008dec76806ce0f09d23c
Steph Warsh <swarsh@u.rochester.edu>
2016-11-04 14:32:22 -0400

    create nav
    for use in php include across website

---
+++ inc/nav.php
@@ -0,0 +1,12 @@
+<nav>
+	<ul>
+		<li><a href="#"><img src="img/color-palette48.png" alt="icon"></a></li>
+		<li>
+			<form>
+				Search: <input type="text" name="searchbar">
+				<input type="submit">
+			</form>
+		</li>
+		<li><a href="#"><img src="img/user48.png" alt="user profile picture"></a></li>
+	</ul>
+</nav>
