e540266da66a65f52b50aac296fe58f04b379fc0
Raina Langevin <aniar_langevin@yahoo.com>
2016-11-04 19:20:24 -0400

    Progress bar working

--- js/progressbar.js
+++ js/progressbar.js
@@ -3,12 +3,15 @@ $(function() {
     	var max = $(this).val();
    		$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
 	});
+	
 });
 
-$(function finishBar() {
+function finishBar() {
+
 	$('.progress').val(100);
 	$('progress').each(function() {
     	var max = $(this).val();
    		$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
 	});
-});
+
+}
