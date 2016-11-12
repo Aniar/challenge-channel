b0ed1436554c01b076bc40b4095baff351e8d409
River <jkjk822@yahoo.com.au>
2016-11-12 16:45:40 -0500

    login.html back to login.php for cookie redirect

--- login.php
+++ login.php
@@ -3,29 +3,35 @@
 		<meta charset="utf-8">
 		<title>Log In | Challenge Channel</title>
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script src="js/loginRequest.js"></script>
 	</head>
 	<body id="login">
 		<div class="box">
 			<h1>Challenge Channel</h1>
 			<h2>Back at it</h2>
 
-			<form action="#"  method="post">
+			<form action="validateUser.php"  method="post">
 			  <label>Username:<br>
 			  	<input type="text" name="username" required>
-				<span id="userName-error" class="error"></span>
 			  </label>
 			  <br>
 			  <label>Password:<br>
 			  	<input type="password" name="password" required>
-				<span id="password-error" class="error"></span>
 			  </label>
 			  <br>
 			  <br>
 			  <input type="submit" value="Submit">
 			</form>
-			<p id='message'></p>
-
-			
+			<p class='error'></p>
 		</div>
 	</body>
 </html>
+
+<?php
+	if($_COOKIE["loggedIn"]){
+		#Redirect browser
+		header("Location: profile.php"); 
+		exit();
+	}
+?>
