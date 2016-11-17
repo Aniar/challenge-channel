721c4dfdecce81b4e0a30e63fe4ec9e5fb66664f
Raina Langevin <aniar_langevin@yahoo.com>
2016-11-17 17:14:05 -0500

    Added slider bar

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,17 +1,23 @@
-$(function() {
-  	$('progress').each(function() {
-    	var max = $(this).val();
-   		$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
-	});
-	
+// $(function() {
+//   	$('progress').each(function() {
+//     	var max = $(this).val();
+//    		$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+// 	});
+// 	
+// });
+// 
+// function finishBar() {
+// 
+// 	$('.progress').val(100);
+// 	$('progress').each(function() {
+//     	var max = $(this).val();
+//    		$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+// 	});
+// 
+// }
+
+$("#exbar").slider();
+$("#exbar").on("slide", function(slideEvt) {
+	// alert(slideEvt.value);
+	var current = slideEvt.value;
 });
-
-function finishBar() {
-
-	$('.progress').val(100);
-	$('progress').each(function() {
-    	var max = $(this).val();
-   		$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
-	});
-
-}
