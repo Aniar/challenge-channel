49b070a0b417cbd9db801ced63baecb870b535f8
Steph Warsh <swarsh@u.rochester.edu>
2016-12-04 14:37:04 -0500

    Remove search bar

--- inc/nav.php
+++ inc/nav.php
@@ -8,12 +8,6 @@
 ?>
 <nav class="navbar">
 	<p class="navbar-brand"><a href="#">Challenge Channel</a></p>
-	<form class="navbar-form navbar-left">
-		<div class="form-group">
-			Search: <input type="text" name="searchbar">
-			<input type="submit" class="btn btn-default">
-		</div>
-	</form>
 	<div class="navbar-right">
 		<p class="navbar-text">Signed in as
 			<a href="#" class="navbar-link"><?php echo "{$_COOKIE['loggedIn']}"?></a>
