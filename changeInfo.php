dfae5f1e1531d0d69e19d5a7a02a2b70d1987692
River <jkjk822@yahoo.com.au>
2016-12-04 23:37:43 -0500

    Another lost child of the merge....

--- changeInfo.php
+++ changeInfo.php
@@ -1,81 +1,35 @@
 <?php
 
 	# getting info to connect to the database
-		require'loginInfo.php';
+	require'loginInfo.php';
 
-		# new connection using login stored in "loginInfo.php"
-		$conn = new mysqli($hostAddress, $uname, $pword, $database);
-		if($conn->connect_error) die($conn->connect_error);
+	# new connection using login stored in "loginInfo.php"
+	$conn = new mysqli($hostAddress, $uname, $pword, $database);
+	if($conn->connect_error) die($conn->connect_error);
 
-		# get user info based on username
-		$user = getUser($_COOKIE["loggedIn"], $conn);
+	//TODO error handling when not logged in
+	# get username
+	$username = $_COOKIE['loggedIn'];
+	# get fieldname
+	$field = $_POST['name'];
 
-		if(isset($_POST['fname'])){
-			//first name
-			# new connection using login stored in "loginInfo.php"
-			$conn = new mysqli($hostAddress, $uname, $pword, $database);
-			if($conn->connect_error) die($conn->connect_error);
-			$fname = $_POST['fname'];
+	if(!empty($_POST[$field])){
+		$value = $_POST[$field];
+		$type = "s";
+		if($field=='password')
+			$value = password_hash($_POST[$field], PASSWORD_DEFAULT);
+		else if($field=='age')
+			$type = "i";
 
-			$query = "UPDATE userInfo SET firstName='$fname' WHERE userName='wwallisabc';";
-			$result = $conn->query($query);
-			if(!$result){
-			      die($conn->error);
-			}
-
-		} else if (isset($_POST['lname'])) {
-			//last name
-			$fname = $_POST['lname'];
-			$conn = new mysqli($hostAddress, $uname, $pword, $database);
-			if($conn->connect_error) die($conn->connect_error);
-			
-			
-			$query = "UPDATE userInfo SET lastName='$lname' WHERE userName='wwallisabc';";
-			$result = $conn->query($query);
-			if(!$result){
-			      die($conn->error);
-			}
-
-		} else if (isset($_POST['age'])){
-			//age
-			$conn = new mysqli($hostAddress, $uname, $pword, $database);
-			if($conn->connect_error) die($conn->connect_error);
-			$age = $_POST['age'];
-			
-			$query = "UPDATE userInfo SET age='$age' WHERE userName='wwallisabc';";
-			$result = $conn->query($query);
-			if(!$result){
-			      die($conn->error);
-			}
-
-		} else if (isset($_POST['password'])){
-			//password
-			$conn = new mysqli($hostAddress, $uname, $pword, $database);
-			if($conn->connect_error) die($conn->connect_error);
-			$password = $_POST['password'];
-			
-			$query = "UPDATE userInfo SET password='$password' WHERE userName='wwallisabc';";
-			$result = $conn->query($query);
-			if(!$result){
-			      die($conn->error);
-			}
-		}
-
-		function getUser($username, $conn){
-		# set up query and post it to database
-		$stmt = $conn->prepare("SELECT * FROM userInfo WHERE userName = ?");
-		$stmt->bind_param("s", $username); #? replaced with $username
+		$stmt = $conn->prepare("UPDATE userInfo SET $field=? WHERE userName=?");
+		if(!$stmt) die($conn->error);
+		$stmt->bind_param($type."s", $value, $username); # 's' means string
 		$stmt->execute();
-		if(!$stmt) die ($conn->error);
-
-		# store result
-		$result = $stmt->get_result();
-		$stmt->fetch();
 
 		$stmt->close();
+		$conn->close();
 
-		return $result->fetch_assoc();
-		}
-
+		echo json_encode(true);
+	}
 
 ?>
