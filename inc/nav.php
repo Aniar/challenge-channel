3b47d9cb7be3f60e876d07dadcc97c8859b3de56
Steph Warsh <swarsh@u.rochester.edu>
2016-12-07 17:01:29 -0500

    links in nav now redirect to profile.php

--- inc/nav.php
+++ inc/nav.php
@@ -7,10 +7,10 @@
 	}
 ?>
 <nav class="navbar">
-	<p class="navbar-brand"><a href="#">Challenge Channel</a></p>
+	<p class="navbar-brand"><a href="profile.php">Challenge Channel</a></p>
 	<div class="navbar-right">
 		<p class="navbar-text">Signed in as
-			<a href="#" class="navbar-link"><?php echo "{$_COOKIE['loggedIn']}"?></a>
+			<a href="profile.php" class="navbar-link"><?php echo "{$_COOKIE['loggedIn']}"?></a>
 		</p>
 		<a href="logout.php"><button type="button" class="btn btn-default navbar-btn">Log Out</button></a>
 	</div>
