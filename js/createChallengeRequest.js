76d6c2d28fec13a8e1bbbdecedf84446aee53459
River <jkjk822@yahoo.com.au>
2016-12-07 13:07:13 -0500

    No extra link back to profile from challenge page

--- js/createChallengeRequest.js
+++ js/createChallengeRequest.js
@@ -22,7 +22,7 @@ $(document).ready(function() { // ideas from https://scotch.io/tutorials/submitt
 				.done(function(data) { //on ajax success
 					// if validation error
 					if(data)
-						$('#message').html("Challenge created! <br><a href='profile.php'>Return to profile</a>");
+						$('#message').text("Challenge created!");
 					else
 						$('p.error').text("Error creating challenge.");
 				});
